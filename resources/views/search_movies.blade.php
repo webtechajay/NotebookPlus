23e9304abd59261d6c31448958913cfbc8535b09
Ajay Rathod <theajayrathod05@gmail.com>
2021-03-31 19:53:13 +0530

    some changes in search template

--- resources/views/search_movies.blade.php
+++ resources/views/search_movies.blade.php
@@ -9,13 +9,20 @@
                  @foreach($showMovies as $movies)
 
                  <div style="margin:10px;">
+                    <h3 style="font-style: italic;font-weight:700;tn"> {{$movies->movie_name}} movie search result here:</h3>
+                    <hr>
              <div class="row">
-                <div class="col-sm-6">
+                
+                <div class="col-md-12">
         <a href="{{route('movie.show', $movies->id)}}"><img src="{{url('uploads/movie_photo', $movies->movie_photo)}}" alt="Image"/ width="150"></a>
       </div>
             </div>
             <div class=" col-sm-8 mt-2">
             <strong style="font-style: italic;">{{$movies->movie_name}}</strong>
+            </div>
+            <hr>
+            <div class=" col-sm-8 mt-2">
+            <strong style="font-style: italic;">{{$movies->movie_desc}}</strong>
             </div>
             </div>
 
