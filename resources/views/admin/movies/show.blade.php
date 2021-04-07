c57ee9c4504a9cde2c9f1c1f57b20b8bff4dde5a
Ajay Rathod <theajayrathod05@gmail.com>
2021-04-07 16:24:48 +0530

    UI Changes

--- resources/views/admin/movies/show.blade.php
+++ resources/views/admin/movies/show.blade.php
@@ -6,7 +6,7 @@
         <div class="row ">
             
           <center>  <div class="col-md-8">
-        <div class="card card-header">
+        <div class="card card-header" style="background-color: black;color: white;">
         <h2>{{$showMovies->movie_title}}</h2>
         </div>
         <hr>
@@ -14,12 +14,25 @@
                 <img src="{{url('uploads/movie_photo', $showMovies->movie_photo)}}" width="200">
             </div>
         <div>
-            <h3 class="mt-2">Desc</h3>
-            <hr>
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+            <h3 class="mt-2 card" style="background-color: black;color: white;font-style: italic;">Desc:</h3>
+            </div>
+            </div>
+            <!-- <hr> -->
+            <div class=" row justify-content-center card card-header">
+                <div class="col-md-12">
             <strong>{{$showMovies->movie_desc}}</strong>
+            </div>
+            </div>
         </div>
-        <hr>
-        <div class="mt-3">
+        <!-- <hr> -->
+         <div class="row justify-content-center">
+                <div class="col-md-6">
+            <h3 class="mt-2 card" style="background-color: black;color: white;font-style: italic;">Screen Short Movie Clip:</h3>
+            </div>
+            </div>
+        <div class="mt-2">
            <img src="{{url('uploads/movie_screen_short', $showMovies->movie_screen_short)}}" width="600"> 
         </div>
         <hr>
--- resources/views/search_movies.blade.php
+++ resources/views/search_movies.blade.php
@@ -7,23 +7,27 @@
          @if(isset($showMovies))
         
                  @foreach($showMovies as $movies)
-
-                 <div style="margin:10px;">
-                    <h3 style="font-style: italic;font-weight:700;tn"> {{$movies->movie_name}} movie search result here:</h3>
-                    <hr>
+                    <div class="card card-header" style="margin-top:15px;background-color: black;color: white;">
+                    <h3 style="font-style: italic;font-weight:700;"> {{$movies->movie_name}} movie search result here:</h3>
+                    </div>
+                    
+                 <div  class="card card-header" style="border:1px solid lightgrey;">
              <div class="row">
-                
-                <div class="col-md-12">
+             <div class="col-md-2">
         <a href="{{route('movie.show', $movies->id)}}"><img src="{{url('uploads/movie_photo', $movies->movie_photo)}}" alt="Image"/ width="150"></a>
       </div>
+      <div class="col-md-10">
+          <h3>Desc:</h3>
+            <strong style="font-style: italic;">{{$movies->movie_desc}}</strong>
+      </div>
             </div>
+
+            <div class="row">
             <div class=" col-sm-8 mt-2">
             <strong style="font-style: italic;">{{$movies->movie_name}}</strong>
             </div>
-            <hr>
-            <div class=" col-sm-8 mt-2">
-            <strong style="font-style: italic;">{{$movies->movie_desc}}</strong>
             </div>
+
             </div>
 
                  @endforeach
